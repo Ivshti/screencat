baeab24140e744f101eef5e2294b76ab0825e378
Ivo Georgiev <ivo@linvo.org>
2015-07-19 11:52:21 +0300

    added app menu to get paste working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var menubar = require('menubar')
 var ipc = require('ipc')
+var Menu = require('menu')
 
 var mb = menubar({
   width: 700,
@@ -20,3 +21,16 @@ ipc.on('terminate', function terminate (ev) {
 ipc.on('resize', function resize (ev, data) {
   mb.window.setSize(data.width, data.height)
 })
+
+var template = [{
+  label: "Screencat", submenu: [] 
+}, {
+  label: 'Edit', 
+  submenu: [
+    { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' }, 
+    { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' }, 
+    { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' }, 
+    { label: 'Select All', accelerator: 'Command+A', selector: 'selectAll:' },
+  ]
+}];
+Menu.setApplicationMenu(Menu.buildFromTemplate(template)); 
